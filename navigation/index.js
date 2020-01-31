a7a3f43c94cf6d0a09641ce4794e8a4043de671f
choo121600 <choo121600@gmail.com>
2020-01-31 14:25:34 +0900

    Navigation

---
+++ navigation/index.js
@@ -0,0 +1,94 @@
+import { createAppContainer } from "react-navigation";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class Calendar extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Calendar </Text>
+      </View>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Home </Text>
+      </View>
+    );
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Profile </Text>
+      </View>
+    );
+  }
+}
+
+import Icon from "react-native-vector-icons/Ionicons";
+
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Calendar: {
+      screen: Calendar,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon
+              style={[{ color: tintColor }]}
+              size={25}
+              name={"ios-images"}
+            />
+          </View>
+        ),
+        activeColor: "#ffffff",
+        inactiveColor: "#92c5c2",
+        barStyle: { backgroundColor: "#2c6d6a" }
+      }
+    },
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{ color: tintColor }]} size={25} name={"ios-home"} />
+          </View>
+        )
+      }
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon
+              style={[{ color: tintColor }]}
+              size={25}
+              name={"ios-person"}
+            />
+          </View>
+        ),
+        activeColor: "#ffffff",
+        inactiveColor: "#a3c2fa",
+        barStyle: { backgroundColor: "#2163f6" }
+      }
+    }
+  },
+  {
+    initialRouteName: "Home",
+    activeColor: "#ffffff",
+    inactiveColor: "#bda1f7",
+    barStyle: { backgroundColor: "#6948f4" }
+  }
+);
+
+export default createAppContainer(TabNavigator);
